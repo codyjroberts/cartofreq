f072b6561c3ee1aacb3d1880cf76457f5a28b49a
Cody Roberts <codyjroberts@gmail.com>
2016-11-29 13:32:58 -0600

    Add streamgraph & historical markers
    Thanks @bhamilton292

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ function initMap() {
     radius         = (Math.min(markerWidth, markerHeight) / 2) - 10,
     x              = d3.scaleLinear().range([0, 2 * Math.PI]),
     y              = d3.scaleSqrt().range([0, radius]),
-    color          = d3.scaleOrdinal(d3.schemeCategory10),
+    color          = d3.scaleOrdinal().range(["#FF2D00","#2E0927", "#FF8C00", "#04756F", "#D90000"]),
+    colorSignal    = d3.scaleLinear().domain([0, 100]).range(["#F00","#0F0"]),
     maxDbm         = 115,
     formatNumber   = d3.format(",d"),
     partition      = d3.partition(),
@@ -19,6 +20,7 @@ function initMap() {
     socket         = io.connect(websocketURI),
     signalsTracked = 1,
     panOffset      = 0.0005,
+    historyMarkers = [],
     sensorNames    = [],
     markerSvg      = d3.select("#sensor")
                        .attr("width", markerWidth)
@@ -42,9 +44,34 @@ function initMap() {
       .padAngle(.35);
   }
 
+  function addCircleMarker(coords, signalStrength) {
+    let mark = L.circleMarker(coords, {
+        radius: 5,
+        fillColor: colorSignal(signalStrength),
+        color: "#000",
+        weight: 0,
+        opacity: 1,
+        fillOpacity: 0.5
+    });
+
+    historyMarkers.push(mark);
+    mark.addTo(smap);
+    if (historyMarkers.length > 20) {
+      let oldMark = historyMarkers.shift();
+      smap.removeLayer(oldMark);
+    }
+  }
+
   function updateMarker(data) {
     // Add sensor name if new
     if (!sensorNames.includes(data.name)) sensorNames.push(data.name);
+
+    let temp = 0;
+    let signalStrength = data.children.forEach(i => {
+      temp += Math.min(Math.max(2 * (parseFloat(i.size) + maxDbm), 0), 100);
+    });
+    temp = temp / data.children.length;
+    addCircleMarker([parseFloat(data.lat), parseFloat(data.lng)], temp);
 
     // Set # tracked signals
     signalsTracked = data.children.length;
@@ -81,11 +108,18 @@ function initMap() {
     },
 
     onAdd: function (map) {
-      var container = L.DomUtil.get("config");
+      let container = L.DomUtil.get("config");
 
       function onClick(e) {
         let h = document.getElementById('settings');
-        (h.style.visibility === 'hidden') ? h.style.visibility = 'visible' : h.style.visibility = 'hidden';
+        let s = document.getElementById('stream');
+        if (h.style.display === 'none') {
+          h.style.display = 'block';
+          s.style.display = 'none';
+        } else {
+          h.style.display = 'none';
+          s.style.display = 'block';
+        }
       }
 
       L.DomEvent.addListener(container, 'click', onClick);
@@ -100,16 +134,29 @@ function initMap() {
     },
 
     onAdd: function (map) {
-      var container = L.DomUtil.get("settings");
+      let container = L.DomUtil.get("settings");
+      return container;
+    }
+  });
+
+  let streamGraph = L.Control.extend({
+    options: {
+      position: 'bottomleft'
+    },
+
+    onAdd: function (map) {
+      let container = L.DomUtil.get("stream");
       return container;
     }
   });
 
   let cIcon = new configIcon();
   let cBox = new configBox();
+  let streamOverlay = new streamGraph();
 
   smap.addControl(cIcon);
   smap.addControl(cBox);
+  smap.addControl(streamOverlay);
 
   // Set TileLayer to B/W
   L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
@@ -117,10 +164,75 @@ function initMap() {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(smap);
 
+  let queue = [];					// value array, initially set to zero
+
+  //TODO: Fix keys
+  let stack = d3.stack()
+    .keys(["RF2449.188MHZ","RF2401.938MHZ","RF101.719MHZ","RF105MHZ","RF93.625MHZ"])
+    .order(d3.stackOrderNone)
+    .offset(d3.stackOffsetNone);
+
+  let series = stack(queue);
+  series.reverse();		// reverse for layering purposes
+
+  let width = 1650,
+    height = 150,
+    yScale = 2500,		// available for easy adjusting
+    history = 10;		// number of entries in graph at once
+  // (if adjusting, also need to change d3.area)
+  let xStream = d3.scaleLinear()
+    .domain([0, (history - 1)])
+    .range([0, width]);
+
+  let yStream = d3.scaleLinear()
+    .domain([0, yScale])		// adjustable
+    .range([0, height]);
+
+  let area = d3.area()
+    .curve(d3.curveCatmullRom.alpha(0.5))
+    .x(function(d, i) { return (i*(window.innerWidth + 150) / 10); })	 // TODO: Fix width, 10 entries (history not accessible here)
+    .y0(function(d) { return yStream(yScale/2 - d[1]); })
+    .y1(function(d) { return yStream(yScale/2 + d[1]); });
+
+  let svg = d3.select("#stream").append("svg").attr("width", "100vw").attr("height", height);
+
+  svg.selectAll("path")
+    .data(series)
+    .enter().append("path")
+    .attr("d", area);
+
+  function updateStreamGraph(data) {
+    let keys = data.map(i => { return i.name });
+
+    data = data.reduce((acc, x) => {
+      acc[x.name] = Math.floor(Math.min(Math.max(2 * (parseFloat(x.size) + 115), 0), 100));
+      return acc;
+    }, {});
+
+    if (queue.length < 10) {
+      queue.push(data);
+      return;
+    } else {
+      queue.shift();
+      queue.push(data);
+    }
+
+    let newSeries = stack(queue);
+    newSeries.reverse();
+
+    svg.selectAll("path")
+      .data(newSeries)
+      .transition().attr("d", area).duration(250)
+      .style("fill", d => { return color(d.key); });
+  }
+
   ////////////////
   //   Events   //
   ////////////////
   socket.on('t', data => {
-    if (data != null) updateMarker(data);
- });
+    if (data != null) {
+      updateMarker(data);
+      updateStreamGraph(data.children);
+    }
+  });
 }
